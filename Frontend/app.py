8c0a684a7244b7125a7452310d2efeaf54a6259f
Aflah <72096386+aflah02@users.noreply.github.com>
2023-03-13 00:55:53 +0530

    Progress Bar and Stuff Works

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,13 +1,12 @@
 import streamlit as st
-
 from streamlit_player import st_player
-
+from backend import get_placeholder_qna
 
 def main():
     c1, c2 = st.columns([5, 3])
 
     with c2:
-        playing_bool = True if st.session_state.get("playing_bool", None) is None else st.session_state.playing_bool
+        playing_bool = False if st.session_state.get("playing_bool", None) is None else st.session_state.playing_bool
         st.write(playing_bool)
         options = {
             "events": ["onProgress"],
@@ -19,12 +18,22 @@ def main():
 
 
     with c1:
-        url = st.text_input("Video URL", "https://youtu.be/c9k8K1eII4g")
+        url = st.text_input("Video URL", "https://youtu.be/ylWORyToTo4")
+        ls_qna, chunk_time_stamps = get_placeholder_qna(url)
         event = st_player(url, **options, key="youtube_player")
-        if event:
-            if event[1]['playedSeconds'] > 5:
-                st.write("Played more than 5 seconds")
-                st.session_state.playing_bool = False
+        st.write(event)
+        while event is None or event.data is None or event.data['playedSeconds'] is None:
+            continue
+        print(event.data['playedSeconds'])
+        st.write(ls_qna)
+        st.write(chunk_time_stamps)
+        # print(event["data"])
+
+        # if event:
+        #     if event[1]['playedSeconds'] > 5:
+        #         st.write("Played more than 5 seconds")
+        #         st.session_state.playing_bool = False
+
 
 if __name__ == "__main__":
     main()
---
+++ Frontend/backend.py
@@ -0,0 +1,77 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+import os
+from config_keys import *
+import openai
+
+openai.api_key = open_ai_key
+
+def get_video_id(url):
+    if "youtu.be" in url:
+        video_id = url.split("/")[-1]
+    elif "youtube.com" in url:
+        video_id = url.split("=")[-1]
+    return video_id
+
+def get_transcript(url):
+    video_id = get_video_id(url)
+    transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
+    return transcript
+
+def parse_transcript_into_chunks(transcript, chunk_size=5):
+    max_time = transcript[-1]['start'] + transcript[-1]['duration']
+    time_per_chunk = max_time / chunk_size
+    curr_start = 0
+    curr_end = time_per_chunk
+    chunks = []
+    chunk_time_stamps = []
+    curr_str = ""
+    for i in range(len(transcript)):
+        if transcript[i]['start'] >= curr_end:
+            chunks.append(curr_str)
+            chunk_time_stamps.append((curr_start, curr_end))
+            curr_str = ""
+            curr_start = curr_end
+            curr_end += time_per_chunk
+        curr_str += transcript[i]['text'] + " "
+    return chunks, chunk_time_stamps
+
+def get_question_and_answer(chunks, chunk_time_stamps):
+    all_qna = []
+    for chunk in chunks:
+        resp = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                    {"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": """Generate 5 questions on the following paragraph in the form "Question":, "Answer":. The text is a piece of youtube video transcript so ignore all noisy information which is irrelevant and only focus on the educational material."""},
+                    {"role": "assistant", "content": "Sure provide the piece of text to generate questions from"},
+                    {"role": "user", "content": f"Text: {chunk}"}
+                ]
+            )
+        ls_qna = resp["choices"][0]["message"]["content"].split('\n')
+        # remove all empty strings
+        ls_qna_filtered = []
+        for qna in ls_qna:
+            if qna != '':
+                ls_qna_filtered.append(qna)
+        all_qna.append(ls_qna_filtered)
+    return all_qna, chunk_time_stamps
+
+def get_placeholder_qna(chunks, chunk_time_stamps):
+    ls_qna = []
+    for chunk in chunks:
+        ls_qna.append("Question: What is the question?")
+        ls_qna.append("Answer: What is the answer?")
+    print("Done!")
+    return ls_qna, chunk_time_stamps
+
+# transcript = get_transcript("https://www.youtube.com/watch?v=Z6nkEZyS9nA")
+
+# chunks, chunk_time_stamps = parse_transcript_into_chunks(transcript, chunk_size=5)
+
+# for i in range(len(chunks)):
+#     print("Chunk", i, ":", chunks[i])
+#     print("Chunk", i, "Time Stamps:", chunk_time_stamps[i])
+#     print()
+
+if __name__ == "__main__":
+    get_placeholder_qna("https://youtu.be/ylWORyToTo4")
