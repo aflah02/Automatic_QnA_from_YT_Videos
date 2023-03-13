d2f6cdc4bc4b2b25ce31d0a16b21c8ca23f782a8
Aflah <72096386+aflah02@users.noreply.github.com>
2023-03-13 15:44:48 +0530

    Clean Up

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,39 +1,117 @@
 import streamlit as st
+from backend import *
 from streamlit_player import st_player
-from backend import get_placeholder_qna
+from time import sleep
+from streamlit_extras.stoggle import stoggle
 
-def main():
+def get_questions_and_answers(url):
+    transcript = get_transcript(url)
+    chunks, chunk_time_stamps = parse_transcript_into_chunks(transcript)
+    ls_qna, chunk_time_stamps = get_placeholder_qna(chunks, chunk_time_stamps)
+    # ls_qna, chunk_time_stamps = get_question_and_answer(chunks, chunk_time_stamps)
+    return chunks, ls_qna, chunk_time_stamps
+
+# Define the function for the Welcome Page
+def welcome_page():
+    st.title("Welcome Page")
+    # Add an input box for the user to enter a value
+    input_value = st.text_input("Enter a url:")
+    # Add a button to compute the value and navigate to the Video Display Page
+    if st.button("Compute and Go to Video Display Page"):
+        # Display a spinner while the computation is in progress
+        with st.spinner("Analyzing Video"):
+            results = get_questions_and_answers(input_value)
+        # Set the session state variables for the results and page
+        st.session_state["results"] = results
+        # save URL to session state
+        st.session_state["url"] = input_value
+        st.session_state["page"] = "Video Display Page"
+        st.experimental_rerun()
+
+# Define the function for the Video Display Page
+def video_display_page():
+
+    # Define session state variable
+    if "is_playing" not in st.session_state:
+        st.session_state.is_playing = False
+
+    # Define callback functions
+    def on_pause():
+        st.session_state.is_playing = False
+        st.write("Player is paused")
+
+    def on_play():
+        st.session_state.is_playing = True
+        st.write("Player is playing")
+
+    results = st.session_state.get("results")
+
+    # time_stamps = results[-1]
+
+    # end_times = [time_stamps[i][1] for i in range(len(time_stamps))]
+    # end_times = [5*(i+1) for i in range(len(time_stamps))] if st.session_state.get("end_times", None) is None else st.session_state.end_times
+
+    # paused_at_given_end = [False for i in range(len(end_times))] if st.session_state.get("paused_at_given_end", None) is None else st.session_state.paused_at_given_end
+
+    # st.session_state.end_times = end_times
+    # st.session_state.paused_at_given_end = paused_at_given_end
+
+    st.session_state.last_pause_time = 0 if st.session_state.get("last_pause_time", None) is None else st.session_state.last_pause_time
+
+    st.title("Video Display Page")
+    # 2 columns
     c1, c2 = st.columns([5, 3])
 
     with c2:
         playing_bool = False if st.session_state.get("playing_bool", None) is None else st.session_state.playing_bool
-        st.write(playing_bool)
+        st.session_state.playing_bool = playing_bool
+        st.write(st.session_state.playing_bool)
         options = {
-            "events": ["onProgress"],
-            "progress_interval": 1000,
-            "playing": st.checkbox("Playing", playing_bool),
+            # Onprogress is needed to get the time data
+            "events": ["onPlay", "onPause", "onProgress"],
+            "progress_interval": 500,
+            "playing": st.checkbox("Playing", st.session_state.playing_bool),
+            "controls": False,
+
         }
-        st.session_state.playing_bool = options["playing"]
         st.write(options)
 
 
     with c1:
-        url = st.text_input("Video URL", "https://youtu.be/ylWORyToTo4")
-        ls_qna, chunk_time_stamps = get_placeholder_qna(url)
+        url = st.session_state.get("url")
         event = st_player(url, **options, key="youtube_player")
-        st.write(event)
-        while event is None or event.data is None or event.data['playedSeconds'] is None:
-            continue
-        print(event.data['playedSeconds'])
-        st.write(ls_qna)
-        st.write(chunk_time_stamps)
-        # print(event["data"])
 
-        # if event:
-        #     if event[1]['playedSeconds'] > 5:
-        #         st.write("Played more than 5 seconds")
-        #         st.session_state.playing_bool = False
+        if event.data is not None and 'playedSeconds' in event.data:
+            st.write("Time Played: ")
+            st.write(event.data['playedSeconds'])
+        # Check if player event was triggered
+        if event.name == "onPause" and st.session_state.playing_bool == True:
+            on_pause()
+            st.session_state.playing_bool = False
+            st.experimental_rerun()
+        elif event.name == "onPlay" and st.session_state.playing_bool == False:
+            on_play()
+            st.session_state.playing_bool = True
+            st.experimental_rerun()
+        qnas = results[1]
+        for qna in qnas:
+            c = 0
+            while c < len(qna):
+                stoggle(qna[c], qna[c+1])
+                c += 2
 
+        
 
+# Define the main function
+def main():
+    # Initialize the session state
+    if "page" not in st.session_state:
+        st.session_state["page"] = "Welcome Page"
+    if st.session_state["page"] == "Welcome Page":
+        welcome_page()
+    elif st.session_state["page"] == "Video Display Page":
+        video_display_page()
+
+# Run the app
 if __name__ == "__main__":
     main()
